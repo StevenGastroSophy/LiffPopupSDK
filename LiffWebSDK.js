d1b813f5f339cf869a044e3a18c5fbccac746e96
StevenGastroSophy <kaspersky518@gmail.com>
2020-02-14 15:27:43 +0800

    Upload demo page
    Upload demo page

--- LiffWebSDK.js
+++ LiffWebSDK.js
@@ -12,46 +12,71 @@
  */
 
 class LiffWeb {
-	constructor() {
-		this.liffId = '';
-		this.scopeArray = [];
-		this.setLiffInfo = () => {};
-		window.liffWeb = this;
-	}
+  constructor() {
+    this.liffId = "";
+    this.scopeArray = [];
+    this.setLiffInfo = () => {};
+    window.liffWeb = this;
+  }
 
-	init(liffId, scopeArray) {
-		this.liffId = liffId;
-		this.scopeArray = scopeArray;
-		// Set document.domain to deal with CORS problem
-		// document.domain = 'YOUR_DOMAIN';
-	}
+  init(liffId, scopeArray) {
+    this.liffId = liffId;
+    this.scopeArray = scopeArray;
+  }
 
-	login(callback) {
-		const w = "575";
-		const h = "600";
-		// When the user clicks on a link that opens a new window using window.open. Make the window appear on the same monitor as its' parent.
+  _getWindowSizeString() {
+    const w = "575";
+    const h = "600";
+    // When the user clicks on a link that opens a new window using window.open. Make the window appear on the same monitor as its' parent.
 
-		// window.screenX will give the position of the current monitor screen.
-		// suppose monitor width is 1360
-		// for monitor 1 window.screenX = 0;
-		// for monitor 2 window.screenX = 1360;
-		const dualScreenLeft = window.screenLeft != undefined ? window.screenLeft : window.screenX;
-		const dualScreenTop = window.screenTop != undefined ? window.screenTop : window.screenY;
+    // window.screenX will give the position of the current monitor screen.
+    // suppose monitor width is 1360
+    // for monitor 1 window.screenX = 0;
+    // for monitor 2 window.screenX = 1360;
+    const dualScreenLeft =
+      window.screenLeft != undefined ? window.screenLeft : window.screenX;
+    const dualScreenTop =
+      window.screenTop != undefined ? window.screenTop : window.screenY;
 
-		const width = window.innerWidth ? window.innerWidth : document.documentElement.clientWidth ? document.documentElement.clientWidth : screen.width;
-		const height = window.innerHeight ? window.innerHeight : document.documentElement.clientHeight ? document.documentElement.clientHeight : screen.height;
+    const width = window.innerWidth
+      ? window.innerWidth
+      : document.documentElement.clientWidth
+      ? document.documentElement.clientWidth
+      : screen.width;
+    const height = window.innerHeight
+      ? window.innerHeight
+      : document.documentElement.clientHeight
+      ? document.documentElement.clientHeight
+      : screen.height;
 
-		// same monitor, the center of its parent.
-		const left = ((width / 2) - (w / 2)) + dualScreenLeft;
-		const top = ((height / 2) - (h / 2)) + dualScreenTop;
+    // same monitor, the center of its parent.
+    const left = width / 2 - w / 2 + dualScreenLeft;
+    const top = height / 2 - h / 2 + dualScreenTop;
 
-		const newWindow = window.open(`https://codepen.io/StevenGastroSophy/debug/bGdVeze`, 'Liff', 'width=' + w + ', height=' + h + ', top=' + top + ', left=' + left);
-		if (window.focus) newWindow.focus();
-		this.setLiffInfo = (liffInfo) => {
-			if(typeof newWindow !== 'undefined') {
-				callback(liffInfo);
-				newWindow.close();
-			}
-		}
-	}
+    return `width=${w}, height=${h}, top=${top}, left=${left}`;
+  }
+
+  login(callback = liffInfo => {}) {
+    const newWindow = window.open(
+      `YOUR_LOGIN_POPUP_LINK`,
+      "Liff",
+      this._getWindowSizeString()
+    );
+    if (window.focus) newWindow.focus();
+    this.setLiffInfo = liffInfo => {
+      if (typeof newWindow !== "undefined") {
+        callback(liffInfo);
+        newWindow.close();
+      }
+    };
+  }
+
+  logout() {
+    const newWindow = window.open(
+      `YOUR_LOGOUT_POPUP_LINK`,
+      "Liff",
+      this._getWindowSizeString()
+    );
+    if (window.focus) newWindow.focus();
+  }
 }
